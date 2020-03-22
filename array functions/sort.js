9b8f6e2a2f6d1c3984e5df016c9d15fb3587d3b3
Nathan Quiocson <nathanquiocson@gmail.com>
2020-03-21 23:23:49 -0700

    removed removed sorted boolean from sorted.js

--- array functions/sort.js
+++ array functions/sort.js
@@ -4,31 +4,25 @@
   Output: [-3, 0, 1, 2, 3, 3, 7, 8, 8, 10, 10, 12, 20, 40]
 */
 function sort(inputArray) { 
-    let hold = 0;
-    let sorted = false;     //Set to true if all numbers have been sorted   
-    let swapped = false;    //Set to true if any numbers were swapped
+    let hold = 0; 
+    let swapped = true;    //Set to true if any numbers were swapped
 
-    while(!sorted){ //loop will keep running until the entire array has been sorted 
+    while(swapped){ //loop will continue to run until not a swap is not made
+        swapped = false; 
 
         for(let current = 0; current < inputArray.length; current++){
             
             if(inputArray[current] > inputArray[current + 1]){  //checks if the current element is larger than the next element
 
-                hold = inputArray[current];                     
-                inputArray[current] = inputArray[current+1];
+                hold = inputArray[current];                     //here the current element is stored in hold         
+                inputArray[current] = inputArray[current+1];    //the current element is then switched with the next element
                 inputArray[current+1] = hold;
-                sorted = false;
-                swapped = true;     //swapped is set to true if any swaps were made while iterating through the array
+                swapped = true;     
 
             }
 
-        }
-
-        if(!swapped){   //if no swaps were made during the iteration of the array, then the array must be sorted
-            sorted = true;
-        }
-        
-        swapped = false;    
+        }        
+           
     }
     
     return inputArray;
